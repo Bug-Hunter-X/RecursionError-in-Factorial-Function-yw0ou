43d442962f10f63d47276e5f436d31bf7ee344b5
BugHunter-X <askzenai@gmail.com>
2024-12-26 17:00:04 +0100

    Add bugSolution.py file

---
+++ bugSolution.py
@@ -0,0 +1,11 @@
+def factorial(n):
+    if n < 0:
+        raise ValueError("Factorial is not defined for negative numbers.")
+    elif n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+print(factorial(5)) 
+print(factorial(0))
+print(factorial(-1)) # This will now raise a ValueError
